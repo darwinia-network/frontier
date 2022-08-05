a758708606fd895e438f0e4c89d9426da1f7ead0
bear <boundless.forest@outlook.com>
2022-08-05 15:49:21 +0800

    Companion for 763, remove schema cache

--- template/node/src/service.rs
+++ template/node/src/service.rs
@@ -489,12 +489,6 @@ pub fn new_full(mut config: Configuration, cli: &Cli) -> Result<TaskManager, Ser
 		),
 	);
 
-	task_manager.spawn_essential_handle().spawn(
-		"frontier-schema-cache-task",
-		None,
-		EthTask::ethereum_schema_cache_task(Arc::clone(&client), Arc::clone(&frontier_backend)),
-	);
-
 	#[cfg(feature = "manual-seal")]
 	{
 		let (block_import, sealing) = consensus_result;
