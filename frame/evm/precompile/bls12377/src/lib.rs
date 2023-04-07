a98eac3a6b308596328617f54adb1e2b2e1830e2
echo <hujw77@gmail.com>
2023-04-07 16:33:39 +0800

    Tests

--- frame/evm/precompile/bls12377/src/lib.rs
+++ frame/evm/precompile/bls12377/src/lib.rs
@@ -17,6 +17,9 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
+#[cfg(test)]
+mod tests;
+
 // Arkworks
 use ark_bls12_377::{Bls12_377, Fq, Fq2, Fr, G1Affine, G1Projective, G2Affine, G2Projective};
 use ark_ec::{pairing::Pairing, AffineRepr, CurveGroup, VariableBaseMSM};
@@ -550,70 +553,5 @@ impl Precompile for BLS12377Pairing {
 			exit_status: ExitSucceed::Returned,
 			output: output.to_vec(),
 		})
-	}
-}
-
-#[cfg(test)]
-mod tests {
-	use super::*;
-	use pallet_evm_test_vector_support::test_precompile_test_vectors;
-
-	#[test]
-	fn process_consensus_tests_for_add_g1() -> Result<(), String> {
-		test_precompile_test_vectors::<BLS12377G1Add>("../testdata/bls12377G1Add_matter.json")?;
-		test_precompile_test_vectors::<BLS12377G1Add>("../testdata/bls12377G1Add_zexe.json")?;
-		Ok(())
-	}
-
-	#[test]
-	fn process_consensus_tests_for_mul_g1() -> Result<(), String> {
-		test_precompile_test_vectors::<BLS12377G1Mul>("../testdata/bls12377G1Mul_matter.json")?;
-		test_precompile_test_vectors::<BLS12377G1Mul>("../testdata/bls12377G1Mul_zexe.json")?;
-		Ok(())
-	}
-
-	#[test]
-	fn process_consensus_tests_for_multiexp_g1() -> Result<(), String> {
-		// TODO:: matter tests
-		// test_precompile_test_vectors::<BLS12377G1MultiExp>(
-		// 	"../testdata/bls12377G1MultiExp_matter.json",
-		// )?;
-		test_precompile_test_vectors::<BLS12377G1MultiExp>(
-			"../testdata/bls12377G1MultiExp_zexe.json",
-		)?;
-		Ok(())
-	}
-
-	#[test]
-	fn process_consensus_tests_for_add_g2() -> Result<(), String> {
-		test_precompile_test_vectors::<BLS12377G2Add>("../testdata/bls12377G2Add_matter.json")?;
-		test_precompile_test_vectors::<BLS12377G2Add>("../testdata/bls12377G2Add_zexe.json")?;
-		Ok(())
-	}
-
-	#[test]
-	fn process_consensus_tests_for_mul_g2() -> Result<(), String> {
-		test_precompile_test_vectors::<BLS12377G2Mul>("../testdata/bls12377G2Mul_matter.json")?;
-		test_precompile_test_vectors::<BLS12377G2Mul>("../testdata/bls12377G2Mul_zexe.json")?;
-		Ok(())
-	}
-
-	#[test]
-	fn process_consensus_tests_for_multiexp_g2() -> Result<(), String> {
-		// TODO:: matter tests
-		// test_precompile_test_vectors::<BLS12377G2MultiExp>(
-		// 	"../testdata/bls12377G2MultiExp_matter.json",
-		// )?;
-		test_precompile_test_vectors::<BLS12377G2MultiExp>(
-			"../testdata/bls12377G2MultiExp_zexe.json",
-		)?;
-		Ok(())
-	}
-
-	#[test]
-	fn process_consensus_tests_for_pairing() -> Result<(), String> {
-		test_precompile_test_vectors::<BLS12377Pairing>("../testdata/bls12377Pairing_matter.json")?;
-		test_precompile_test_vectors::<BLS12377Pairing>("../testdata/bls12377Pairing_zexe.json")?;
-		Ok(())
 	}
 }
---
+++ frame/evm/precompile/bls12377/src/tests.rs
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: Apache-2.0
+// This file is part of Frontier.
+//
+// Copyright (c) 2020-2022 Parity Technologies (UK) Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use super::*;
+use pallet_evm_test_vector_support::test_precompile_test_vectors;
+
+#[test]
+fn process_consensus_tests_for_add_g1() -> Result<(), String> {
+	test_precompile_test_vectors::<BLS12377G1Add>("../testdata/bls12377G1Add_matter.json")?;
+	test_precompile_test_vectors::<BLS12377G1Add>("../testdata/bls12377G1Add_zexe.json")?;
+	Ok(())
+}
+
+#[test]
+fn process_consensus_tests_for_mul_g1() -> Result<(), String> {
+	test_precompile_test_vectors::<BLS12377G1Mul>("../testdata/bls12377G1Mul_matter.json")?;
+	test_precompile_test_vectors::<BLS12377G1Mul>("../testdata/bls12377G1Mul_zexe.json")?;
+	Ok(())
+}
+
+#[test]
+fn process_consensus_tests_for_multiexp_g1() -> Result<(), String> {
+	// TODO:: matter tests
+	// test_precompile_test_vectors::<BLS12377G1MultiExp>(
+	// 	"../testdata/bls12377G1MultiExp_matter.json",
+	// )?;
+	test_precompile_test_vectors::<BLS12377G1MultiExp>("../testdata/bls12377G1MultiExp_zexe.json")?;
+	Ok(())
+}
+
+#[test]
+fn process_consensus_tests_for_add_g2() -> Result<(), String> {
+	test_precompile_test_vectors::<BLS12377G2Add>("../testdata/bls12377G2Add_matter.json")?;
+	test_precompile_test_vectors::<BLS12377G2Add>("../testdata/bls12377G2Add_zexe.json")?;
+	Ok(())
+}
+
+#[test]
+fn process_consensus_tests_for_mul_g2() -> Result<(), String> {
+	test_precompile_test_vectors::<BLS12377G2Mul>("../testdata/bls12377G2Mul_matter.json")?;
+	test_precompile_test_vectors::<BLS12377G2Mul>("../testdata/bls12377G2Mul_zexe.json")?;
+	Ok(())
+}
+
+#[test]
+fn process_consensus_tests_for_multiexp_g2() -> Result<(), String> {
+	// TODO:: matter tests
+	// test_precompile_test_vectors::<BLS12377G2MultiExp>(
+	// 	"../testdata/bls12377G2MultiExp_matter.json",
+	// )?;
+	test_precompile_test_vectors::<BLS12377G2MultiExp>("../testdata/bls12377G2MultiExp_zexe.json")?;
+	Ok(())
+}
+
+#[test]
+fn process_consensus_tests_for_pairing() -> Result<(), String> {
+	test_precompile_test_vectors::<BLS12377Pairing>("../testdata/bls12377Pairing_matter.json")?;
+	test_precompile_test_vectors::<BLS12377Pairing>("../testdata/bls12377Pairing_zexe.json")?;
+	Ok(())
+}
